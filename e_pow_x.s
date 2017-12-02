12470432e267c086bf47f93999e6373be028a1aa
anishunni333 <33221363+anishunni333@users.noreply.github.com>
2017-12-02 15:09:44 +0530

    Update e_pow_x.s

--- e_pow_x.s
+++ e_pow_x.s
@@ -4,20 +4,21 @@
      EXPORT __main
 	 ENTRY 
 __main  FUNCTION
-	MVF f1, #3.0;x-Numberin e^x
-	MVF f2, #8.0;Number of terms in e^x expansion
-	MVF f3, #1.0;count
-	MVF f4, #1.0; temp
-	MVF f5, #1.0; result
-Loop
-	 CMFSZ f2, f3;
+	VMOV.F32 S1, #4;x-Number for find e^x
+	VMOV.F32 S2, #15;Number of terms to be considered in e^x expansion
+	VMOV.F32 S3, #1;count
+	VMOV.F32 S4, #1; temp
+	VMOV.F32 S5, #1; result
+	VMOV.F32 S7, #1;register for holding one
+Loop 
+	 VCMP.F32 S2, S3;
+	 VMRS.F32 APSR_nzcv,FPSCR;Used to copy fpscr to apsr
 	 BLT stop;
-	 DVFSZ f6, f1, f3;
-	 MUFSZ f4, f4, f6;
-	 ADF f5, f5, f4;
-	 FIXSZ r0, f5; 
-	 ADF f3, f3, #1.0;
-	 B Loop; 
+	 VDIV.F32 S6, S1, S3; 
+	 VMUL.F32 S4, S4, S6; 
+	 VADD.F32 S5, S5, S4; 
+	 VADD.F32 S3, S3, S7; 
+	 B Loop;  
 	 
 	 
 stop B stop ; stop program
